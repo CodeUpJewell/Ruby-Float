4962f59e18076150f02d019e66e32e2747ba903a
Jewell Borders <94193721+CodeUpJewell@users.noreply.github.com>
2023-03-07 21:11:31 -0600

    Add files via upload

---
+++ float_find_hypotenuse.rb
@@ -0,0 +1,16 @@
+# A right triangle's two shorter sides have length 2.8 and 4.5. How long is its longest side?
+# Check the pythagorous.png file for the Pythagorean Theorem
+#
+# Your output should be a number; for example,
+#
+# 5.3
+
+a = 2.8 ** 2
+
+b = 4.5 ** 2
+
+c = (a + b) ** 0.5
+
+p c = (( a**2 ) + (b ** 2)) ** 0.5
+
+p c
---
+++ float_round.rb
@@ -0,0 +1,5 @@
+# Output ten divided by 3 rounded to 3 decimal places.
+
+answer = 10 / 3.000
+
+p answer.round(3)
